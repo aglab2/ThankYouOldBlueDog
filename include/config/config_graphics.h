02b748a2b684b9596a9dbb27214e83480e959a70
aglab2 <aglab3@gmail.com>
2024-05-04 10:16:16 +0800

    Added graphics options

--- include/config/config_graphics.h
+++ include/config/config_graphics.h
@@ -55,17 +55,17 @@
 /**
  * Disables AA (Improves console performance but causes visible seams between unconnected geometry).
  */
-// #define DISABLE_AA
+#define DISABLE_AA
 
 /**
  * Makes the coins ia8 64x64 instead of ia16 32x32. Uses new ia8 textures so that vanilla coins look better.
  */
-// #define IA8_COINS
+#define IA8_COINS
 
 /**
  * Similar to the above, but 30 FPS (Textures by InTheBeef, cleaned up by Arceveti).
  */
-// #define IA8_30FPS_COINS
+#define IA8_30FPS_COINS
 
 /**
  * Use .rej microcode for certain objects (experimental - only should be used when F3DEX_GBI_2 is defined).
