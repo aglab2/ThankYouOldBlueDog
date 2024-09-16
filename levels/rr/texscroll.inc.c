f98269ecfffe613cb50b04bd05d0bbec8f781486
aglab2 <aglab3@gmail.com>
2024-09-16 22:48:10 +0800

    More RR work

--- levels/rr/texscroll.inc.c
+++ levels/rr/texscroll.inc.c
@@ -166,62 +166,25 @@ void scroll_rr_dl_ow_013_mesh_layer_5_vtx_0() {
 	currentY += deltaY;
 }
 
-void scroll_rr_dl_Sphere_mesh_layer_1_vtx_0() {
+void scroll_rr_dl_Plane_012_mesh_layer_5_vtx_0() {
 	int i = 0;
-	int count = 1722;
+	int count = 315;
 	int width = 32 * 0x20;
-	int height = 32 * 0x20;
 
 	static int currentX = 0;
 	int deltaX;
-	static int currentY = 0;
-	int deltaY;
-	Vtx *vertices = segmented_to_virtual(rr_dl_Sphere_mesh_layer_1_vtx_0);
+	Vtx *vertices = segmented_to_virtual(rr_dl_Plane_012_mesh_layer_5_vtx_0);
 
-	deltaX = (int)(0.10000002384185791 * 0x20) % width;
-	deltaY = (int)(-0.13999998569488525 * 0x20) % height;
+	deltaX = (int)(0.550000011920929 * 0x20) % width;
 
 	if (absi(currentX) > width) {
 		deltaX -= (int)(absi(currentX) / width) * width * signum_positive(deltaX);
 	}
-	if (absi(currentY) > height) {
-		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
-	}
 
 	for (i = 0; i < count; i++) {
 		vertices[i].n.tc[0] += deltaX;
-		vertices[i].n.tc[1] += deltaY;
 	}
-	currentX += deltaX;	currentY += deltaY;
-}
-
-void scroll_rr_dl_Sphere_001_mesh_layer_1_vtx_0() {
-	int i = 0;
-	int count = 1729;
-	int width = 32 * 0x20;
-	int height = 32 * 0x20;
-
-	static int currentX = 0;
-	int deltaX;
-	static int currentY = 0;
-	int deltaY;
-	Vtx *vertices = segmented_to_virtual(rr_dl_Sphere_001_mesh_layer_1_vtx_0);
-
-	deltaX = (int)(0.10000002384185791 * 0x20) % width;
-	deltaY = (int)(-0.13999998569488525 * 0x20) % height;
-
-	if (absi(currentX) > width) {
-		deltaX -= (int)(absi(currentX) / width) * width * signum_positive(deltaX);
-	}
-	if (absi(currentY) > height) {
-		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
-	}
-
-	for (i = 0; i < count; i++) {
-		vertices[i].n.tc[0] += deltaX;
-		vertices[i].n.tc[1] += deltaY;
-	}
-	currentX += deltaX;	currentY += deltaY;
+	currentX += deltaX;
 }
 
 void scroll_rr_dl_Sphere_002_mesh_layer_1_vtx_0() {
@@ -436,8 +399,7 @@ void scroll_rr() {
 	scroll_rr_dl_ow_009_mesh_layer_5_vtx_0();
 	scroll_rr_dl_ow_011_mesh_layer_5_vtx_0();
 	scroll_rr_dl_ow_013_mesh_layer_5_vtx_0();
-	scroll_rr_dl_Sphere_mesh_layer_1_vtx_0();
-	scroll_rr_dl_Sphere_001_mesh_layer_1_vtx_0();
+	scroll_rr_dl_Plane_012_mesh_layer_5_vtx_0();
 	scroll_rr_dl_Sphere_002_mesh_layer_1_vtx_0();
 	scroll_rr_dl_Sphere_003_mesh_layer_1_vtx_0();
 	scroll_rr_dl_Sphere_004_mesh_layer_1_vtx_0();
