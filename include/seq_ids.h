946b47f2c54cd4e582ca197be87a8b225855d106
aglab2 <aglab3@gmail.com>
2024-09-17 20:42:30 +0800

    HMC done

--- include/seq_ids.h
+++ include/seq_ids.h
@@ -45,6 +45,9 @@ enum SeqId {
     SEQ_EVENT_CUTSCENE_ENDING,        // 0x20
     SEQ_MENU_FILE_SELECT,             // 0x21
     SEQ_EVENT_CUTSCENE_LAKITU,        // 0x22 (not in JP)
+
+    SEQ_LEVEL_LOOMING,
+
     SEQ_COUNT
 };
 
--- levels/hmc/script.c
+++ levels/hmc/script.c
@@ -41,7 +41,7 @@ const LevelScript level_hmc_entry[] = {
 		OBJECT(MODEL_STAR, -14, 4490, -1626, 0, 0, 0, 0x00000000, bhvStar),
 		TERRAIN(hmc_area_1_collision),
 		MACRO_OBJECTS(hmc_area_1_macro_objs),
-		SET_BACKGROUND_MUSIC(0x00, SEQ_LEVEL_GRASS),
+		SET_BACKGROUND_MUSIC(0x00, SEQ_LEVEL_LOOMING),
 		TERRAIN_TYPE(TERRAIN_GRASS),
 		/* Fast64 begin persistent block [area commands] */
 		/* Fast64 end persistent block [area commands] */
