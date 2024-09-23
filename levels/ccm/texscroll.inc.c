2d4d7b1b11d026c298d58456a66f233daf8206e5
aglab2 <aglab3@gmail.com>
2024-09-23 20:42:47 +0800

    Added indicators for flippers

--- levels/ccm/texscroll.inc.c
+++ levels/ccm/texscroll.inc.c
@@ -1,10 +1,7 @@
-
-extern u32 gIsGravityFlipped;
-
 void scroll_gfx_ccm_dl_Plane_003_mesh_layer_1() {
 	Gfx *mat = segmented_to_virtual(ccm_dl_Plane_003_mesh_layer_1);
 
-	shift_s(mat, 17, PACK_TILESIZE(0, gIsGravityFlipped ? 1 : -1));
+	shift_s(mat, 17, PACK_TILESIZE(0, 1));
 
 };
 
